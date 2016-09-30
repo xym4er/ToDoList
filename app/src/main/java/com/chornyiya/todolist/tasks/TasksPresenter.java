115bf0b13f446fec29f1248de05908c6ff6ca6d2
y.chornyi <xym4er@gmail.com>
2016-09-30 15:51:46 +0300

    123

--- app/src/main/java/com/chornyiya/todolist/tasks/TasksPresenter.java
+++ app/src/main/java/com/chornyiya/todolist/tasks/TasksPresenter.java
@@ -1,8 +1,14 @@
 package com.chornyiya.todolist.tasks;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 
 import com.chornyiya.todolist.data.Task;
+import com.chornyiya.todolist.data.source.TasksDataSource;
+import com.chornyiya.todolist.data.source.TasksRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -16,6 +22,10 @@ public class TasksPresenter implements TasksContract.Presenter {
 
     private final TasksContract.View mTasksView;
 
+    private TasksFilterType mCurrentFiltering = TasksFilterType.ALL_TASKS;
+
+    private boolean mFirstLoad = true;
+
     public TasksPresenter(@NonNull TasksRepository tasksRepository, @NonNull TasksContract.View tasksView) {
         mTasksRepository = checkNotNull(tasksRepository, "tasksRepository cannot be null");
         mTasksView = checkNotNull(tasksView, "tasksView cannot be null!");
@@ -25,12 +35,84 @@ public class TasksPresenter implements TasksContract.Presenter {
 
     @Override
     public void result(int requestCode, int resultCode) {
-
+        // If a task was successfully added, show snackbar
+        if (AddEditTaskActivity.REQUEST_ADD_TASK == requestCode && Activity.RESULT_OK == resultCode) {
+            mTasksView.showSuccessfullySavedMessage();
+        }
     }
 
     @Override
     public void loadTasks(boolean forceUpdate) {
+        // Simplification for sample: a network reload will be forced on first load.
+        loadTasks(forceUpdate || mFirstLoad, true);
+        mFirstLoad = false;
+    }
 
+    private void loadTasks(boolean forceUpdate, final boolean showLoadingUI) {
+        if (showLoadingUI) {
+            mTasksView.setLoadingIndicator(true);
+        }
+        if (forceUpdate) {
+            mTasksRepository.refreshTasks();
+        }
+
+        // The network request might be handled in a different thread so make sure Espresso knows
+        // that the app is busy until the response is handled.
+        EspressoIdlingResource.increment(); // App is busy until further notice
+
+        mTasksRepository.getTasks(new TasksDataSource.LoadTasksCallback() {
+            @Override
+            public void onTasksLoaded(List<Task> tasks) {
+                List<Task> tasksToShow = new ArrayList<Task>();
+
+                // This callback may be called twice, once for the cache and once for loading
+                // the data from the server API, so we check before decrementing, otherwise
+                // it throws "Counter has been corrupted!" exception.
+                if (!EspressoIdlingResource.getIdlingResource().isIdleNow()) {
+                    EspressoIdlingResource.decrement(); // Set app as idle.
+                }
+
+                // We filter the tasks based on the requestType
+                for (Task task : tasks) {
+                    switch (mCurrentFiltering) {
+                        case ALL_TASKS:
+                            tasksToShow.add(task);
+                            break;
+                        case ACTIVE_TASKS:
+                            if (task.isActive()) {
+                                tasksToShow.add(task);
+                            }
+                            break;
+                        case COMPLETED_TASKS:
+                            if (task.isCompleted()) {
+                                tasksToShow.add(task);
+                            }
+                            break;
+                        default:
+                            tasksToShow.add(task);
+                            break;
+                    }
+                }
+                // The view may not be able to handle UI updates anymore
+                if (!mTasksView.isActive()) {
+                    return;
+                }
+                if (showLoadingUI) {
+                    mTasksView.setLoadingIndicator(false);
+                }
+
+                processTasks(tasksToShow);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                // The view may not be able to handle UI updates anymore
+                if (!mTasksView.isActive()) {
+                    return;
+                }
+                mTasksView.showLoadingTasksError();
+            }
+        });
     }
 
     @Override
@@ -70,6 +152,6 @@ public class TasksPresenter implements TasksContract.Presenter {
 
     @Override
     public void start() {
-
+        loadTasks(false);
     }
 }
