f17f0bbcfe4e431c053c71d8d6c36473997bb403
y.chornyi <xym4er@gmail.com>
2016-10-02 13:45:08 +0300

    123

--- app/src/main/java/com/chornyiya/todolist/tasks/TasksFragment.java
+++ app/src/main/java/com/chornyiya/todolist/tasks/TasksFragment.java
@@ -1,8 +1,135 @@
 package com.chornyiya.todolist.tasks;
 
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.chornyiya.todolist.data.Task;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by y.chornyi on 27.09.2016.
  */
 
-public class TasksFragment {
+public class TasksFragment extends Fragment implements TasksContract.View {
+
+    private TasksContract.Presenter mPresenter;
+
+    private TasksAdapter mListAdapter;
+
+    private View mNoTasksView;
+
+    private ImageView mNoTaskIcon;
+
+    private TextView mNoTaskMainView;
+
+    private TextView mNoTaskAddView;
+
+    private LinearLayout mTasksView;
+
+    private TextView mFilteringLabelView;
+
+    public TasksFragment() {
+        // Requires empty public constructor
+    }
+
+    public static TasksFragment newInstance() {
+        return new TasksFragment();
+    }
+
+
+    @Override
+    public void setLoadingIndicator(boolean active) {
+
+    }
+
+    @Override
+    public void showTasks(List<Task> tasks) {
+
+    }
+
+    @Override
+    public void showAddTask() {
+
+    }
+
+    @Override
+    public void showTaskDetailsUi(String taskId) {
+
+    }
+
+    @Override
+    public void showTaskMarkedComplete() {
+
+    }
+
+    @Override
+    public void showTaskMarkedActive() {
+
+    }
+
+    @Override
+    public void showCompletedTasksCleared() {
+
+    }
+
+    @Override
+    public void showLoadingTasksError() {
+
+    }
+
+    @Override
+    public void showNoTasks() {
+
+    }
+
+    @Override
+    public void showActiveFilterLabel() {
+
+    }
+
+    @Override
+    public void showCompletedFilterLabel() {
+
+    }
+
+    @Override
+    public void showAllFilterLabel() {
+
+    }
+
+    @Override
+    public void showNoActiveTasks() {
+
+    }
+
+    @Override
+    public void showNoCompletedTasks() {
+
+    }
+
+    @Override
+    public void showSuccessfullySavedMessage() {
+
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public void showFilteringPopUpMenu() {
+
+    }
+
+    @Override
+    public void setPresenter(TasksContract.Presenter presenter) {
+        mPresenter = checkNotNull(presenter);
+    }
 }
--- app/src/main/java/com/chornyiya/todolist/tasks/TasksPresenter.java
+++ app/src/main/java/com/chornyiya/todolist/tasks/TasksPresenter.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import com.chornyiya.todolist.data.Task;
 import com.chornyiya.todolist.data.source.TasksDataSource;
 import com.chornyiya.todolist.data.source.TasksRepository;
+import com.chornyiya.todolist.util.EspressoIdlingResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -115,39 +116,88 @@ public class TasksPresenter implements TasksContract.Presenter {
         });
     }
 
+    private void processTasks(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            // Show a message indicating there are no tasks for that filter type.
+            processEmptyTasks();
+        } else {
+            // Show the list of tasks
+            mTasksView.showTasks(tasks);
+            // Set the filter label's text.
+            showFilterLabel();
+        }
+    }
+
+    private void showFilterLabel() {
+        switch (mCurrentFiltering) {
+            case ACTIVE_TASKS:
+                mTasksView.showActiveFilterLabel();
+                break;
+            case COMPLETED_TASKS:
+                mTasksView.showCompletedFilterLabel();
+                break;
+            default:
+                mTasksView.showAllFilterLabel();
+                break;
+        }
+    }
+
+    private void processEmptyTasks() {
+        switch (mCurrentFiltering) {
+            case ACTIVE_TASKS:
+                mTasksView.showNoActiveTasks();
+                break;
+            case COMPLETED_TASKS:
+                mTasksView.showNoCompletedTasks();
+                break;
+            default:
+                mTasksView.showNoTasks();
+                break;
+        }
+    }
+
     @Override
     public void addNewTask() {
-
+        mTasksView.showAddTask();
     }
 
     @Override
     public void openTaskDetails(@NonNull Task requestedTask) {
-
+        checkNotNull(requestedTask, "requestedTask cannot be null!");
+        mTasksView.showTaskDetailsUi(requestedTask.getId());
     }
 
     @Override
     public void completeTask(@NonNull Task completedTask) {
-
+        checkNotNull(completedTask, "completedTask cannot be null!");
+        mTasksRepository.completeTask(completedTask);
+        mTasksView.showTaskMarkedComplete();
+        loadTasks(false, false);
     }
 
     @Override
     public void activateTask(@NonNull Task activeTask) {
-
+        checkNotNull(activeTask, "activeTask cannot be null!");
+        mTasksRepository.activateTask(activeTask);
+        mTasksView.showTaskMarkedActive();
+        loadTasks(false, false);
     }
 
     @Override
     public void clearCompletedTasks() {
-
+        mTasksRepository.clearCompletedTasks();
+        mTasksView.showCompletedTasksCleared();
+        loadTasks(false, false);
     }
 
     @Override
     public void setFiltering(TasksFilterType requestType) {
-
+        mCurrentFiltering = requestType;
     }
 
     @Override
     public TasksFilterType getFiltering() {
-        return null;
+        return mCurrentFiltering;
     }
 
     @Override
